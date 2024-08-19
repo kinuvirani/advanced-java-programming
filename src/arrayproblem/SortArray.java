a874f31da5c0aeb6a11d331a1e0cfd87509d309f
kiran-lns <kiran@lockandstock.app>
2024-08-19 14:05:46 -0400

    Sort array

---
+++ src/arrayproblem/SortArray.java
@@ -0,0 +1,29 @@
+package arrayproblem;
+
+import java.util.Scanner;
+
+public class SortArray {
+    static boolean isSorted(int [] intArray) {
+        for (int i = 0; i < intArray.length; i++) {
+            for (int j = i + 1; j < intArray.length; j++) {
+                if (intArray[i] > intArray[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an array length::");
+        int size = scanner.nextInt();
+        System.out.println("Please enter an array element::");
+        int [] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Check whether array is sorted or not.!");
+        System.out.println("Array is Sorted? " + isSorted(arr));
+    }
+}
