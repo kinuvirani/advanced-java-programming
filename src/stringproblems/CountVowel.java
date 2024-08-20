75c39d210503968394a4b552a218ca539329cb37
kiran-lns <kiran@lockandstock.app>
2024-08-20 09:09:51 -0400

    fix count vowel problem.

--- src/stringproblems/CountVowel.java
+++ src/stringproblems/CountVowel.java
@@ -3,7 +3,9 @@ package stringproblems;
 import java.util.Scanner;
 
 public class CountVowel {
+    static int [] vowelOccurences = new int [5];
 
+    //Validate the input string
     static boolean isValidateInputString(String str) {
         if (str == "" || str == null) {
             return false;
@@ -11,18 +13,54 @@ public class CountVowel {
         return true;
     }
 
-    static int countVowels(String inputStr) {
+    //Get the vowel occurences
+    static int getVowelCount(int index) {
+        return vowelOccurences[index] == 0 ? 1 : (vowelOccurences[index] = vowelOccurences[index] + 1);
+    }
+
+    //Count vowels
+    static String countVowels(String inputStr) {
         char [] charArray = inputStr.toLowerCase().toCharArray();
         char [] vowelArray = {'a', 'e', 'i', 'o', 'u'};
+
+        StringBuilder sb = new StringBuilder();
         int count = 0;
         for (char ch: charArray) {
             for (int i = 0; i < vowelArray.length; i++) {
                 if (ch == vowelArray[i]) {
+                    sb.append(ch);
                     count++;
+                    switch (ch) {
+                        case 'a':
+                            vowelOccurences[0] = getVowelCount(0);
+                            break;
+                        case 'e':
+                            vowelOccurences[1] = getVowelCount(1);
+                            break;
+                        case 'i':
+                            vowelOccurences[2] = getVowelCount(2);
+                            break;
+                        case 'o':
+                            vowelOccurences[3] = getVowelCount(3);
+                            break;
+                        case 'u':
+                            vowelOccurences[4] = getVowelCount(4);
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
         }
-        return count;
+
+        StringBuilder vowelOccurencesSb = new StringBuilder();
+
+        for (int i = 0; i < vowelOccurences.length; i++) {
+            vowelOccurencesSb.append("{" + vowelArray[i] + ":" + vowelOccurences[i] + "}");
+        }
+
+        return "Count = "+ count + "\n" + "Vowels = " + sb.toString() + "\n"
+                + "Total vowel occurences are:: " + vowelOccurencesSb;
     }
 
     public static void main(String[] args) {
@@ -33,7 +71,7 @@ public class CountVowel {
         if (!isValidInput) {
             return;
         } else {
-            System.out.println("Count is= " + countVowels(str));
+            System.out.println(countVowels(str));
         }
     }
 }
